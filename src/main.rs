3a5b64402eaa1fc4c429c3b8078180359c31e15a
Yanming Tang <ytang@Yanming-Hive-MacBook-Pro.local>
2024-12-04 20:14:13 -0800

    Test commit

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     let connection = Connection::open("./analysis.db").unwrap();
     // limit_analysis::scrape::datasets::fetch_and_save_all_datasets(&connection).await;
     // limit_analysis::scrape::dataset_info::fetch_and_save_all_datasets_info(&connection).await;
-    limit_analysis::scrape::dataset_stats::fetch_and_save_all_datasets_stats(&connection).await;
+    limit_analysis::scrape::dataset_stats::fetch_and_save_datasets_missing_stats(&connection).await;
 
     // limit_analysis::analysis::dataset_rows::get_dataset_row_limit_coverage(&connection, "downloads", 100_000);
 }
