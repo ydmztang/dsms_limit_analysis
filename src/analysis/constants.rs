31b3b6cbd31d9194e4fd8f2a37eef5a80f23e5f0
Yanming Tang <ytang@Yanming-Hive-MacBook-Pro.local>
2024-12-11 09:30:08 -0800

    Add analysis for label cardinality

--- src/analysis/constants.rs
+++ src/analysis/constants.rs
@@ -1,2 +1,3 @@
 // Report the number every {GRANULARITY} percentage of datasets
 pub const GRANULARITY: f32 = 0.01;
+pub const MAX_CARDINALITY: usize = 1000;
--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -2,5 +2,6 @@ pub mod constants;
 pub mod dataset_rows;
 pub mod dataset_size;
 pub mod image_size;
+pub mod label_cardinality;
 pub mod max_columns;
 pub mod max_field_length;
